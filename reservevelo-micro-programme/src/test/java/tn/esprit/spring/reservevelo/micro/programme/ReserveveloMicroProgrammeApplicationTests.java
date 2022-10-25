fd82791da90126e4e5c55bcc92a937a8147ec9eb
AchrefBarketi <a.barketi@sapres.de>
2022-10-25 19:42:31 +0100

    balade

---
+++ reservevelo-micro-programme/src/test/java/tn/esprit/spring/reservevelo/micro/programme/ReserveveloMicroProgrammeApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring.reservevelo.micro.programme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveveloMicroProgrammeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
